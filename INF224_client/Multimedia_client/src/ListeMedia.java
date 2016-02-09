140df4c5c585727d5868172b39b73aa995e89d98
Simon Fabry <sfabry@c129-08.enst.fr>
2016-02-09 15:00:39 +0100

    Fin du TP

---
+++ INF224_client/Multimedia_client/src/ListeMedia.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+
+public class ListeMedia extends ArrayList<Multimedia> {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
---
+++ INF224_client/Multimedia_client/src/Multimedia.java
@@ -0,0 +1,21 @@
+
+public class Multimedia {
+
+	private String nom;
+	
+	private String type;
+
+	public Multimedia(String nom, String type) {
+		this.nom = nom;
+		this.type = type;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+}
